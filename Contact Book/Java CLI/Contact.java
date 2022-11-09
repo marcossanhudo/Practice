3f2762044f9233df8e1088a0c0cc9ecc2b0008d2
Marcos Sanhudo <marcossanhudo@protonmail.com>
2022-11-09 14:27:18 -0300

    Creates usage options menu. Creates welcome message. Starts contact addition interface. Fixes some errors. Improves code structure.

--- Contact Book/Java CLI/Contact.java
+++ Contact Book/Java CLI/Contact.java
@@ -1,5 +1,3 @@
-package ContactBook;
-
 public class Contact {
 
 	// Variables
--- Contact Book/Java CLI/ContactBook.java
+++ Contact Book/Java CLI/ContactBook.java
@@ -1,51 +1,108 @@
-package ContactBook;
-
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import ContactBook.Contact.*;
+import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class ContactBook {
 
+	public static ArrayList<Contact> contactsL = new ArrayList<>();
 	public static Contact[] contacts;
+	public static File contactListFile;
 
 	public static void main(String[] args){
-		int language = chooseLanguage();
+		Scanner globalScanner = new Scanner(System.in);
+		int language = chooseLanguage(globalScanner);
 		welcomeMessage(language);
-		menu(language);
+		menu(language, globalScanner);
 	}
 
-	public static int chooseLanguage(){
-		Scanner scanner = new Scanner(System.in);
+	public static int chooseLanguage(Scanner globalScanner){
 		int userInput = -1;
-		int languageCount = 5;
-		while (userInput < 0 && userInput > languageCount){
+		int languageCount = 6;
+		while (userInput < 1 || userInput > languageCount){
 			System.out.println("1 to use this application in English");
 			System.out.println("2 para usar esta aplicação em português");
 			System.out.println("3 pour utiliser cette application en français");
 			System.out.println("4 für diese Application in Deutsch nutzen");
 			System.out.println("5 を入力するとこのアプリケーションを日本語で使える");
 			System.out.println("6 을 입력해면 이 애플리케이션을 한국어로 사용할 수 있다");
-			userInput = scanner.nextInt();
-			if (userInput >= 0 && userInput < languageCount) return userInput;
-		}	
+
+			userInput = globalScanner.nextInt();
+
+			if (userInput >= 1 && userInput < languageCount) return userInput;
+		}
+		return 0;	
 	}
 
 	public static void welcomeMessage(int language){
-		;
+		System.out.println();
+		System.out.println("Welcome.");
+		System.out.println("Today is " + LocalDate.now() + ".");
+		System.out.println("You have " + contactsL.size() + " contacts saved.");
+		System.out.println();
 	}
-	public static void menu(int language){
-		File contactList = new File("./contactList.txt");
-		try (Scanner scanner = new Scanner(System.in)){
-			
-			
+
+	public static void menu(int language, Scanner globalScanner){
+		int userInput = -1;
+		while (userInput != 0){
+			System.out.println("What would you like to do?");
+			System.out.println("0\tEnd the application");
+			System.out.println("1\tSee all my contacts");
+			System.out.println("2\tSearch for a contact");
+			System.out.println("3\tAdd a contact");
+			System.out.println("4\tDelete all my contacts");
+
+			userInput = globalScanner.nextInt();
+
+			switch (userInput){
+				case 0: break;
+				case 1: listContacts(language); break;
+				case 2: contactSearchInterface(language); break;
+				case 3: contactAdditionInterface(language); break;
+				case 4: allContactDeletionInterface(language); break;
+			}
+		}
+	}
+
+	public static void findsContactsInLocalDirectory(){
+		contactListFile = new File("./contactList.txt");
+		try (Scanner localScanner = new Scanner(System.in)){
+			while (localScanner.hasNextLine()){
+				String[] strings = localScanner.nextLine().split("\t");
+				// contact info extraction
+			}
 		} catch (FileNotFoundException e){
 			System.out.println(e);
 		}
 	}
 
-	public static void contactAdditionInterface(){}
-	public static boolean addContact(String name, Phone[] phones, Email[] emails, Place[] places, File contactList){
+	
+	public static boolean listContacts(int language){
+		for (int index = 0; index < contacts.length; index++)
+			System.out.println("[" + index + "] " + contacts[index].getName() + contacts[index].getPhones()[0]);
+		return true;
+	}
+	public static void contactAdditionInterface(int language, Scanner globalScanner){
+		System.out.println("Inform your new contact's name.");
+		String name = globalScanner.nextLine();
+
+		System.out.println("Would you like to add a phone?");
+		Contact.Phone[] phones = new Contact.Phone[0];
+
+		System.out.println("Would you like to add an email?");
+		Contact.Email[] emails = new Contact.Email[0];
+
+		System.out.println("Would you like to add a place?");
+		Contact.Place[] places = new Contact.Place[0];
+		
+		if (addContact(name, phones, emails, places))
+			System.out.println("\nYour new contact was successfully added to your contact list.");
+		else
+			System.out.println("Something happened, and it was not possible to add your new contact to your contact list.");
+	}
+
+	public static boolean addContact(String name, Contact.Phone[] phones, Contact.Email[] emails, Contact.Place[] places){
 		Contact newContact = new Contact(name);
 		newContact.setPhones(phones);
 		newContact.setEmails(emails);
@@ -56,23 +113,28 @@ public class ContactBook {
 	public static boolean saveNewContact(Contact newContact, File file){
 		try {
 			//file.write(newContact);
+			contactsL.add(newContact);
 			return true;
 		} catch (Exception e) {
 			return false;
 		}
 	}
 
-	public static boolean listContacts(){
-		for (int index = 0; index < contacts.length; index++)
-			System.out.println("[" + index + "] " + contacts[index].getName() + contacts[index].getPhones()[0]);
+
+	public static void allContactDeletionInterface(int language){}
+	public static boolean deleteAllContacts(){
+		return true;
 	}
 
-	public static void allContactDeletionInterface(){}
-	public static boolean deleteAllContacts(){}
+	public static void contactSearchInterface(int language){}
 
-	public static void contactEditionInterface(){}
-	public static boolean editContact(){}
+	public static void contactEditionInterface(int language){}
+	public static boolean editContact(){
+		return true;
+	}
 
-	public static void contactDeletionInterface(){}
-	public static boolean deleteContact(){}
+	public static void contactDeletionInterface(int language){}
+	public static boolean deleteContact(){
+		return true;
+	}
 }
