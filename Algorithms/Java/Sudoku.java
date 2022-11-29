46036f1922b60e779295242998b720fb1440b130
Marcos Sanhudo <113375194+marcossanhudo@users.noreply.github.com>
2022-11-29 16:17:15 -0300

    Adds Portuguese (Brazil) translation. Improves English (United States) version.
    Adiciona tradução em português (Brasil). Melhora versão inglesa (Estados Unidos).
    Ajoute d'une traduction en portugais (Brésil). Améliore version anglaise (États Unis).

--- Algorithms/Java/Sudoku.java
+++ Algorithms/Java/Sudoku.java
@@ -1,16 +1,21 @@
 /*
+	English (United States) version first.
+	Versão portuguesa (Brasil) mais abaixo.
+	Version française (France) ci-dessous.
+	
+	English (United States)
 		This algorithm will try to solve a given incomplete sudoku problem,
 	using the backtracking paradigm.
 		Backtracking is a strategy where, given that the decisions of an algorithm
 	can be represented as branches in a tree, an algorithm iterates through branches of decisions,
 	and discards each branch it finds to be incapable of solving the given problem.
-	The search performed by this algorithm can be classified as "Depth-First Search" ("DFS"),
-	because the algorithm tries each branch, as close to finishing the branch as possible;
+	The search performed by this algorithm can be classified as "Depth-First Search" (abbreviated "DFS"),
+	because the algorithm analyzes each branch, as close to finishing the branch as possible;
 	if that branch is finished -- or, in the case of backtracking, discarded --,
-	the algorithm will search through all other remaining branches, ordered by their closest shared ancestor.
+	the algorithm will analyze all remaining branches, ordered by their closest shared ancestor.
 		Here, backtracking will try to solve the sudoku puzzle, space by space,
 	by trying every possible digit, but only proceding if that digit can, according to the rules of the puzzle,
-	be inserted in that space; if, after that insertion, if the algorithm later finds out
+	be inserted in that space; if, after that insertion, the algorithm finds out
 	that there's no digit that can be inserted while respecting the rules,
 	the algorithm will discard that insertion, in favor of a next one. If no insertion is able
 	to give a correct answer, the sudoku problem is unsolvable (or the algorithm is poorly written).
@@ -116,3 +121,23 @@ public class Sudoku {
 	}
 		
 }
+/*
+	Português (Brasil)
+		Este algoritmo tentará resolver um dado problema incompleto de Sudoku,
+	usando o paradigma de backtracking.
+		Backtracking é uma estratégia, onde, dado que as decisões de um algoritmo
+	podem ser representadas como ramos de uma árvore, um algoritmo itera sobre ramos de decisões,
+	e descarta cada ramo que ele descobre ser incapaz de resolver o dado problema.
+	A pesquisa desempenhada por este algoritmo pode ser classificada como "Depth-First Search" (abreviado "DFS"; traduzível como "Busca em Profundidade"),
+	porque o algoritmo analisa cada ramo, o mais próximo possível de terminá-lo;
+	se esse ramo for terminado -- ou, no caso do backtracking, descartado --,
+	o algoritmo analisará todos os ramos pendentes, na ordem do mais próximo ancestral comum.
+		Aqui, o backtracking tentará resolver o sudoku, espaço por espaço,
+	tentando todo dígito possível, mas apenas prosseguindo se esse dígito puder, de acordo com as regras do jogo,
+	ser inserso nesse espaço; se, depois desta inserção, o algoritmo descobrir
+	que não há dígito que possa ser inserso com respeito às regras,
+	o algoritmo descartará essa inserção, em favor de uma próxima. Se nenhuna inserção é capaz
+	de dar uma resposta correta, o problema de sudoku é insolucionável (ou o algoritmo é mal escrito).
+		Este algoritmo utilizará o tipo primitivo int do Java, que não oferece valores nulos ou vazios.
+	Logo, o int 0 representará a vaziedade de um espaço.
+*/
