a5cf413a479b81e2b556fdcf7abeb6cbfc0ff2a7
Marcos Sanhudo <>
2022-12-02 14:32:04 -0300

    Adds more functions.
    Adiciona mais funções.
    
    Ajoute plus functions.

--- Statistician/Java CLI/Statistician.java
+++ Statistician/Java CLI/Statistician.java
@@ -9,13 +9,16 @@ public class Statistician {
 		if (args.length == 0)
 			throw new Exception("Inform the numbers you want to analyse on the command line.");
 		parseArguments(args);
-		System.out.println("Amplitude: " + amplitude());
+		System.out.println("Sample size (n): " + args.length);
+		System.out.println("Amplitude (R): " + amplitude());
 		System.out.println("\tHighest: " + highest());
 		System.out.println("\tLowest: " + lowest());
-		System.out.println("Average: " + average());
-		System.out.println("Mean: " + mean());
-		System.out.println("Mode: " + mode());
-		//System.out.println("Standard deviation: ");
+		System.out.println("Arithmetic average (x̄): " + arithmeticAverage());
+		System.out.println("Median (Md): " + median());
+		System.out.println("Mode (Mo): " + mode());
+		System.out.println("Sample variance (s²): " + sampleVariance());
+		//System.out.println("Sample standard deviation (s): " + standardDeviation());
+		//System.out.println();
 	}
 
 	public static void parseArguments(String args[]){
@@ -61,10 +64,10 @@ public class Statistician {
 		return highest(set) - lowest(set);
 	}
 
-	public static double average(){
-		return average(data);
+	public static double arithmeticAverage(){
+		return arithmeticAverage(data);
 	}
-	public static double average(double[] set){
+	public static double arithmeticAverage(double[] set){
 		double numerator = 0;
 		for (double entry: set)
 			numerator += entry;
@@ -92,19 +95,66 @@ public class Statistician {
 		return mode;
 	}
 
-	public static double mean(){
-		return mean(data);
+	public static double median(){
+		return median(data);
 	}
-	public static double mean(double[] set){
+	public static double median(double[] set){
 		int setLength = set.length;
 		if (setLength % 2 == 0){
 			double[] setMiddle = {set[setLength / 2], set[setLength / 2 + 1]};
-			return average(setMiddle);
+			return arithmeticAverage(setMiddle);
 		} else
 			return set[setLength / 2];
 	}
 
-	/*public static double[] inAscending(){
+	public static double sampleVariance(){
+		return sampleVariance(data);
+	}
+	public static double sampleVariance(double[] set){
+		double variance = 0;
+		double arithmeticAverage = arithmeticAverage();
+		double setLength = set.length;
+		for (int index = 0; index < setLength; index++){
+			variance += exponentiation(set[index] - arithmeticAverage, 2);
+		}
+		variance /= setLength - 1;
+		return variance;
+	}
+
+	public static double exponentiation(double base, double exponent){
+		if (base == 0) return 0;
+		if (exponent == 0) return 1;
+		for (int iteration = 1; iteration < exponent; iteration++){
+			base *= base;
+		}
+		return base;
+	}
+	/*public static double radiciation(double base, double root){
+		
+	}
+	public static double factorisation(double toFactor){
+		int factor = 2;
+		if (number % factor == 0) {
+			number 
+		}
+		else factor = nextPrime(factor);
+	}
+	public static int nextPrime(int currentNumber){
+		for (int test = currentNumber; ; test++){
+			for (int previousNumber = 2; previousNumber < test; previousNumber++)
+				if (test % previousNumber == 0) break;
+			return test;
+		}
+	}
+
+	public static double standardDeviation(){
+		return standardDeviation(data);
+	}
+	public static double standardDeviation(double[] set){
+		return 0;
+	}
+
+	public static double[] inAscending(){
 		
 	}
 	public static double[] inDescending(){
